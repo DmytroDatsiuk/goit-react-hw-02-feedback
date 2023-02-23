69cfa420a575dfedce90352647eceac9f6dbba17
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2023-02-23 07:00:11 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { AppBox, StatisticTitle } from './App.styled';
+import { FeedbackOptions } from './Feadback/FeadbackOptions';
+import { GlobalStyle } from './Global.styled';
+import { Notification } from './Notification/Notification';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistic/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    const currentBtnValue = e.currentTarget.value;
+    this.setState(prevState => ({
+      ...prevState,
+      [currentBtnValue]: prevState[currentBtnValue] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const valuesArr = Object.values(this.state);
+    return valuesArr.reduce((acc, val) => {
+      acc += val;
+      return acc;
+    }, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.trunc((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    const btnName = Object.keys(this.state);
+
+    return (
+      <AppBox>
+        <GlobalStyle />
+        <Section title="Please leave feadback">
+          <FeedbackOptions
+            options={btnName}
+            onLeaveFeedback={this.onLeaveFeedback}
+          ></FeedbackOptions>
+          <StatisticTitle>Statistic</StatisticTitle>
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </AppBox>
+    );
+  }
+}
---
+++ src/components/Statistic/Statistics.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { Item, List } from './Statistic.styled';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => (
+  <List>
+    <Item>Good: {good}</Item>
+    <Item>Neutral: {neutral}</Item>
+    <Item>Bad: {bad}</Item>
+    <Item>Total: {total}</Item>
+    <Item>Positive: {positivePercentage}%</Item>
+  </List>
+);
+
+Statistics.prototype = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.func.isRequired,
+};
